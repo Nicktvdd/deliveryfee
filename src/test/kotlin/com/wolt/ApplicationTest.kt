059ac7d5935fa32031ba3eb3bb70a13f35c80247
Nick van den Dungen <nicktvdd@hotmail.com>
2024-01-25 16:16:22 +0200

    creating tests

--- src/test/kotlin/com/wolt/ApplicationTest.kt
+++ src/test/kotlin/com/wolt/ApplicationTest.kt
@@ -9,13 +9,15 @@ import kotlin.test.*
 
 class ApplicationTest {
 	@Test
-	fun testRoot() = testApplication {
+	fun testApi() {
 		val requestBody = """{"cart_value": 790, "delivery_distance": 2235, "number_of_items": 4, "time": "2024-01-15T13:00:00Z"}"""
 
+		val jsonData = jacksonObjectMapper().
 		application {
 			configureRouting()
 		}
-		client.post("/delivery-fee").apply {
+		client.post("/api/delivery-fee").apply {
+			setBody(requestBody)
 			assertEquals(HttpStatusCode.OK, status)
 			assertEquals("application/json; charset=UTF-8", contentType().toString())
 		}
