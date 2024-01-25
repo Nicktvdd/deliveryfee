f85e3b2b623cbdf1de5bf5b04b772ba692a04643
nicktvdd <nicktvdd@gmail.com>
2024-01-25 09:05:26 +0200

    testing and parsing todo

--- src/main/kotlin/com/wolt/routes/DeliveryFeeRoute.kt
+++ src/main/kotlin/com/wolt/routes/DeliveryFeeRoute.kt
@@ -8,13 +8,15 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
 
 private const val BASE_URL = "http://localhost:8080" //make this dynamic somehow?
 
 fun Route.deliveryFee() {
-		route("/delivery-fee") {
+		route("/api/delivery-fee") {
 			post {
-				val request = call.receive<DeliveryRequest>()
+				val unparsedrequest = call.receive<DeliveryRequest>()
+				val request = Json.decodeFromString<unparsedrequest>(jsonString)
 
 				val deliveryFee = calculateDeliveryFee(request)
 				call.respond(DeliveryResponse(deliveryFee))
