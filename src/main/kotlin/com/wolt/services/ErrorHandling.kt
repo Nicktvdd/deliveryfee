c353ad82acc2b94ab415a3f3fa58260b46ac266d
nicktvdd <nicktvdd@gmail.com>
2024-01-26 12:57:23 +0200

    Number of items

--- src/main/kotlin/com/wolt/services/ErrorHandling.kt
+++ src/main/kotlin/com/wolt/services/ErrorHandling.kt
@@ -8,4 +8,6 @@ import io.ktor.server.response.*
 suspend fun errorHandling(call: ApplicationCall, request: DeliveryRequest) {
 	if (request.cartValue < 0)
 		call.respond(HttpStatusCode.BadRequest, "Invalid cart value")
+	if (request.numberOfItems <= 0)
+		call.respond(HttpStatusCode.BadRequest, "Invalid amount of items")
 }
--- src/test/kotlin/com/wolt/ApplicationTest.kt
+++ src/test/kotlin/com/wolt/ApplicationTest.kt
@@ -37,5 +37,18 @@ class CustomerTests {
 		}
 		assertEquals("Invalid cart value", response.bodyAsText())
 		assertEquals(HttpStatusCode.BadRequest, response.status)
+	}	@Test
+	fun testBadItemAmount() = testApplication {
+		application {
+			configureSerialization()
+			configureRouting() // Replace this with the actual configuration of your routes
+		}
+
+		val response = client.post("/api/delivery-fee") {
+			contentType(ContentType.Application.Json)
+			setBody("""{"cart_value": 790, "delivery_distance": 2235, "number_of_items": 0, "time": "2024-01-15T13:00:00Z"}""")
+		}
+		assertEquals("Invalid amount of items", response.bodyAsText())
+		assertEquals(HttpStatusCode.BadRequest, response.status)
 	}
 }
