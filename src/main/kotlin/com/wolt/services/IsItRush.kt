3eec5653616b4b0b48ea9ebc7a43a5a6c8c77e31
nicktvdd <nicktvdd@gmail.com>
2024-01-27 19:31:11 +0200

    Catch in rush

--- src/main/kotlin/com/wolt/services/IsItRush.kt
+++ src/main/kotlin/com/wolt/services/IsItRush.kt
@@ -3,22 +3,25 @@ package com.wolt.services
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.DayOfWeek
+import java.time.format.DateTimeParseException
 
 fun isItRush(request: String): Boolean {
 	val formatter = DateTimeFormatter.ISO_DATE_TIME
-	val dateTime = LocalDateTime.parse(request, formatter)
-
-	return if (dateTime.dayOfWeek == DayOfWeek.FRIDAY) {
-		val startOfRush = dateTime
-			.withHour(15)
-			.withMinute(0)
-			.withSecond(0)
-		val endOfRush = dateTime
-			.withHour(19)
-			.withMinute(0)
-			.withSecond(0)
-		return dateTime.isAfter(startOfRush) && dateTime.isBefore(endOfRush)
-	} else {
-		false
+	try {
+		val dateTime = LocalDateTime.parse(request, formatter)
+		return if (dateTime.dayOfWeek == DayOfWeek.FRIDAY) {
+			val startOfRush = dateTime
+				.withHour(15)
+				.withMinute(0)
+				.withSecond(0)
+			val endOfRush = dateTime
+				.withHour(19)
+				.withMinute(0)
+				.withSecond(0)
+			return dateTime.isAfter(startOfRush) && dateTime.isBefore(endOfRush)
+		} else {
+			false
+		} } catch (e: DateTimeParseException){
+		return false
 	}
 }
